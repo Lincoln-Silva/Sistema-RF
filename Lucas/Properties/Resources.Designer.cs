fff59d6615e422890ebe093a4adadae0c8a3a450
Lincoln <71925693+Lincoln-Silva@users.noreply.github.com>
2021-11-21 16:46:57 -0300

    system

--- Lucas/Properties/Resources.Designer.cs
+++ Lucas/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Lucas.Properties {
+namespace system.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Lucas.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lucas.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("system.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
